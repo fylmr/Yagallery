fa7b216db35adddd35cd8f9ad4dbfc620b8d0756
Philipp Maksimov <filipp.39@gmail.com>
2018-04-10 16:59:01 +0200

    Vk Login added

--- app/src/main/java/com/example/fylmr/ya_gallery/MyApp.kt
+++ app/src/main/java/com/example/fylmr/ya_gallery/MyApp.kt
@@ -1,11 +1,27 @@
 package com.example.fylmr.ya_gallery
 
 import android.app.Application
+import android.util.Log
+import com.vk.sdk.VKAccessToken
+import com.vk.sdk.VKAccessTokenTracker
 import com.vk.sdk.VKSdk
 
+
 class MyApp : Application() {
+    val TAG = "MyApp"
+
+
+    var vkAccessTokenTracker: VKAccessTokenTracker = object : VKAccessTokenTracker() {
+        override fun onVKAccessTokenChanged(oldToken: VKAccessToken?, newToken: VKAccessToken?) {
+            if (newToken == null) {
+                Log.e(TAG, "VK Login error")
+            }
+        }
+    }
+
     override fun onCreate() {
-        VKSdk.initialize(applicationContext)
         super.onCreate()
+        vkAccessTokenTracker.startTracking()
+        VKSdk.initialize(applicationContext)
     }
 }
--- app/src/main/java/com/example/fylmr/ya_gallery/activities/MainActivity.kt
+++ app/src/main/java/com/example/fylmr/ya_gallery/activities/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.fylmr.ya_gallery.activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
 import com.example.fylmr.ya_gallery.R
+import com.vk.sdk.VKAccessToken
+import com.vk.sdk.VKCallback
 import com.vk.sdk.VKScope
 import com.vk.sdk.VKSdk
+import com.vk.sdk.api.VKError
+
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,11 +22,32 @@ class MainActivity : AppCompatActivity() {
 
         VKSdk.login(this, VKScope.PHOTOS)
 
-//        skipToNextActvity()
+
     }
 
     private fun skipToNextActvity() {
         val goToGalleryIntent = Intent(applicationContext, GalleryActivity::class.java)
         startActivity(goToGalleryIntent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data,
+                        object : VKCallback<VKAccessToken> {
+                            override fun onResult(res: VKAccessToken) {
+                                skipToNextActvity()
+                            }
+
+                            override fun onError(error: VKError) {
+                                showLoginError(error)
+                            }
+                        }
+                )) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun showLoginError(error: VKError) {
+        Log.e(TAG, "VK Login error")
+        Toast.makeText(applicationContext, error.errorMessage, Toast.LENGTH_SHORT).show()
     }
 }
