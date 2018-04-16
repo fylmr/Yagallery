9e0756d178264fcea6f352d1b7166f14faf504b8
Philipp Maksimov <filipp.39@gmail.com>
2018-04-16 21:45:56 +0200

    Added Moxy strategies

--- app/src/main/java/com/example/fylmr/ya_gallery/views/GalleryView.kt
+++ app/src/main/java/com/example/fylmr/ya_gallery/views/GalleryView.kt
@@ -2,11 +2,16 @@ package com.example.fylmr.ya_gallery.views
 
 import android.content.Intent
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.example.fylmr.ya_gallery.entities.Picture
 
 interface GalleryView: MvpView {
     fun populateGallery(pics: MutableList<Picture>)
+
+    @StateStrategyType(SkipStrategy::class)
     fun photoClicked(picture: Picture)
 
+    @StateStrategyType(SkipStrategy::class)
     fun openActivityForResult(intent: Intent)
 }
